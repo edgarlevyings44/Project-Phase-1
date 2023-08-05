752c2f3be32fe38e7d2b80e90b1600d4e999561b
Levy <edgarlevy81@gmail.com>
2023-08-06 01:35:12 +0300

    7th Commit

--- script.js
+++ script.js
@@ -6,6 +6,9 @@
       .then(data => {
         return data;
       })
+      .then(quotes => {
+        displayRandomQuote(quotes);
+      })
       .catch(error => {
         console.error('Error fetching quotes:', error);
         return [];
@@ -15,10 +18,33 @@
   // Function to display random quotes
 
 function displayRandomQuote(quotes) {
-  const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    
   const quoteText = document.getElementById('quote-text');
   const quoteAuthor = document.getElementById('quote-author');
+  const getQuoteButton = document.getElementById('get-quote-button');
 
-  quoteText.textContent = quotes[randomQuoteIndex].text;
-  quoteAuthor.textContent = "- " + quotes[randomQuoteIndex].author;
+getQuoteButton.addEventListener('click', () => {
+    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    quoteText.textContent = quotes[randomQuoteIndex].text;
+    quoteAuthor.textContent = "- " + quotes[randomQuoteIndex].author;
+})
+  
+  
+  let numLikes = 0;
+  let numDislikes = 0;
+  const likeButton = document.getElementById('like-button');
+  const dislikeButton = document.getElementById('dislike-button');
+
+  likeButton.addEventListener('click', () => {
+      numLikes++;
+      likeButton.classList.add('liked');
+  });
+
+  dislikeButton.addEventListener('click', () => {
+      numDislikes++;
+      dislikeButton.classList.add('disliked');
+  });
+
 }
+displayRandomQuote(fetchQuotes());
+
